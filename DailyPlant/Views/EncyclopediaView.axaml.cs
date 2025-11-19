de4097203bc625ecb47c178b4dd38918ed96c82b
dyc191 <1917176089@qq.com>
2025-11-19 20:53:50 +0800

    Add files via upload

---
+++ DailyPlant/Views/EncyclopediaView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DailyPlant.Views;
+
+public partial class EncyclopediaView : UserControl
+{
+    public EncyclopediaView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ DailyPlant/Views/PhotoRecognitionView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DailyPlant.Views;
+
+public partial class PhotoRecognitionView : UserControl
+{
+    public PhotoRecognitionView()
+    {
+        InitializeComponent();
+    }
+}
