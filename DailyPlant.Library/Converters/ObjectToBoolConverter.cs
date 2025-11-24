f4bcade62f620222267188b34130f0262d31602b
xinyu-parasol <964478541@qq.com>
2025-11-24 15:51:43 +0800

    Add files via upload

---
+++ DailyPlant.Library/Converters/ObjectToBoolConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace DailyPlant.Converters
+{
+    public class ObjectToBoolConverter : IValueConverter
+    {
+        // 单例实例
+        public static ObjectToBoolConverter IsNotNull { get; } = new ObjectToBoolConverter();
+        public static ObjectToBoolConverter IsNull { get; } = new ObjectToBoolConverter { _invert = true };
+
+        private bool _invert = false;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = value != null;
+            return _invert ? !result : result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ DailyPlant.Library/Converters/StringIsNotNullOrEmptyConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace DailyPlant.Library.Converters
+{
+    public class StringIsNotNullOrEmptyConverter : IValueConverter
+    {
+        // 单例实例
+        public static StringIsNotNullOrEmptyConverter Instance { get; } = new StringIsNotNullOrEmptyConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrEmpty(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
