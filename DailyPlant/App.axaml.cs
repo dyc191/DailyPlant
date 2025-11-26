5d42f63a47dff734afb18fd3c145e5e732209d15
Makabaca666 <3352815193@qq.com>
2025-11-26 16:43:35 +0800

    Add files via upload

---
+++ DailyPlant/App.axaml.cs
@@ -0,0 +1,42 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using Avalonia.Markup.Xaml;
+using DailyPlant.Library.Services;
+using DailyPlant.Library.ViewModels;
+
+using DailyPlant.Views;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
+
+namespace DailyPlant;
+
+public partial class App : Application 
+{
+    public override void Initialize() 
+    {
+        AvaloniaXamlLoader.Load(this);
+        
+        if (!Resources.ContainsKey("ServiceLocator"))
+        {
+            Resources.Add("ServiceLocator", new ServiceLocator());
+        }
+    }
+
+    public override void OnFrameworkInitializationCompleted() {
+        
+        IconProvider.Current
+            .Register<FontAwesomeIconProvider>();
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) 
+        {
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = ServiceLocator.Current.MainWindowViewModel
+            };
+
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ DailyPlant/ServiceLocator.cs
@@ -0,0 +1,84 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using DailyPlant.Library.Services;
+using DailyPlant.Library.ViewModels;
+using DailyPlant.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyPlant;
+
+public class ServiceLocator {
+    private readonly IServiceProvider _serviceProvider;
+
+    private static ServiceLocator _current;
+
+    public static ServiceLocator Current {
+        get
+        {
+            if (_current is not null) {
+                return _current;
+            }
+
+            if (Application.Current.TryGetResource(nameof(ServiceLocator),
+                    out var resource) &&
+                resource is ServiceLocator serviceLocator) {
+                return _current = serviceLocator;
+            }
+
+            throw new Exception("理论上来讲不应该发生这种情况。");
+        }
+    }
+
+    public InitializationViewModel InitializationViewModel =>
+        _serviceProvider.GetService<InitializationViewModel>();
+
+    public MainWindowViewModel MainWindowViewModel =>
+        _serviceProvider.GetService<MainWindowViewModel>();
+
+    public MainViewModel MainViewModel =>
+        _serviceProvider.GetService<MainViewModel>();
+    
+    public TodayPlantViewModel TodayPlantViewModel =>
+        _serviceProvider.GetService<TodayPlantViewModel>();
+    
+    public EncyclopediaViewModel EncyclopediaViewModel =>
+        _serviceProvider.GetService<EncyclopediaViewModel>();
+    
+    public  TakePhotoViewModel TakePhotoViewModel =>
+        _serviceProvider.GetService<TakePhotoViewModel>();
+    
+    public PlantDetailViewModel PlantDetailViewModel =>
+        _serviceProvider.GetService<PlantDetailViewModel>();
+    
+    
+
+
+    public ServiceLocator() {
+        var serviceCollection = new ServiceCollection();
+
+        serviceCollection.AddSingleton<InitializationViewModel>();
+        serviceCollection.AddSingleton<MainWindowViewModel>();
+        serviceCollection.AddSingleton<MainViewModel>();
+        
+        serviceCollection.AddSingleton<TodayPlantViewModel>();
+        serviceCollection.AddSingleton<EncyclopediaViewModel>();
+        serviceCollection.AddSingleton<TakePhotoViewModel>();
+        serviceCollection.AddSingleton<PlantDetailViewModel>();
+        
+        
+        
+
+        serviceCollection
+            .AddSingleton<IRootNavigationService, RootNavigationService>();
+        serviceCollection
+            .AddSingleton<IMenuNavigationService, MenuNavigationService>();
+        serviceCollection
+            .AddSingleton<IContentNavigationService, ContentNavigationService>();
+        
+        serviceCollection.AddSingleton<DailyService>();
+        
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+         
+    }
+}
