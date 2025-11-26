f0e054f8854913d6ccdcee6b3041698e6c04ce93
Makabaca666 <3352815193@qq.com>
2025-11-26 16:42:27 +0800

    Add files via upload

---
+++ DailyPlant/Services/ContentNavigationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DailyPlant.Library.Services;
+using DailyPlant.Library.ViewModels;
+
+namespace DailyPlant.Services;
+
+public class ContentNavigationService : IContentNavigationService {
+    public void NavigateTo(string view, object parameter = null) {
+        ViewModelBase viewModel = view switch {
+            ContentNavigationConstant.TodayPlantView => ServiceLocator.Current
+                .TodayPlantViewModel,
+            ContentNavigationConstant.TakePhotoView => ServiceLocator.Current
+                .TakePhotoViewModel,
+            ContentNavigationConstant.EncyclopediaView => ServiceLocator.Current
+                .EncyclopediaViewModel,
+            ContentNavigationConstant.PlantDetailView => ServiceLocator.Current
+                .PlantDetailViewModel,
+            _ => throw new Exception("未知的视图。")
+        };
+
+        if (parameter != null) {
+            viewModel.SetParameter(parameter);
+        }
+
+        ServiceLocator.Current.MainViewModel.PushContent(viewModel);
+    }
+}
