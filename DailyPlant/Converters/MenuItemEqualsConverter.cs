49a71bea17a8dfb44dc80b98847a2f2df1172732
xinyu-parasol <964478541@qq.com>
2025-11-26 21:23:31 +0800

    Add files via upload

---
+++ DailyPlant/Converters/MenuItemEqualsConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace DailyPlant.Converters
+{
+    public class MenuItemEqualsConverter : IValueConverter
+    {
+        public static MenuItemEqualsConverter Instance { get; } = new MenuItemEqualsConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString() == parameter?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
