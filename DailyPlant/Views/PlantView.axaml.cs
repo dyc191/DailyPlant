90dce7efd9092ff63d751d977af39b895d20858e
dyc191 <1917176089@qq.com>
2025-11-27 21:21:49 +0800

    Add files via upload

---
+++ DailyPlant/Views/PlantView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DailyPlant.Views
+{
+    public partial class PlantView : UserControl
+    {
+        public PlantView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
