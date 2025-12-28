6a4fb1b8f7c029528b2aabcc91adb48f725276e3
Makabaca666 <3352815193@qq.com>
2025-12-28 13:30:21 +0800

    Add files via upload

--- DailyPlant/Views/PlantDetailView.axaml.cs
+++ DailyPlant/Views/PlantDetailView.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using DailyPlant.Library.Services;
 
 namespace DailyPlant.Views;
 
@@ -10,5 +13,42 @@ public partial class PlantDetailView : UserControl
     {
         InitializeComponent();
         DataContext = ServiceLocator.Current.PlantDetailViewModel;
+        
+        // 添加点击外部关闭菜单的事件
+        this.AddHandler(PointerPressedEvent, OnPointerPressed, handledEventsToo: true);
+    }
+    
+    private void OnShareOptionsClick(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var popup = this.FindControl<Popup>("ShareMenuPopup");
+        if (popup != null)
+        {
+            popup.IsOpen = !popup.IsOpen;
+        }
+        e.Handled = true;
+    }
+    
+    private void OnCloseShareMenu(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var popup = this.FindControl<Popup>("ShareMenuPopup");
+        if (popup != null)
+        {
+            popup.IsOpen = false;
+        }
+    }
+    
+    private void OnPointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        // 如果点击的不是菜单按钮，也不是菜单内部，则关闭菜单
+        var shareOptionsButton = this.FindControl<Button>("ShareOptionsButton");
+        var shareMenuPopup = this.FindControl<Popup>("ShareMenuPopup");
+        
+        if (shareOptionsButton != null && shareMenuPopup != null && 
+            shareMenuPopup.IsOpen && 
+            !shareOptionsButton.IsPointerOver && 
+            !shareMenuPopup.IsPointerOver)
+        {
+            shareMenuPopup.IsOpen = false;
+        }
     }
 }
